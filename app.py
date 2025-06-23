47bf23f216dcda3d2b0ab2987e987a0423bcb6e6
WasimEdu <WASIM.20231CSG0054@presidencyuniversity.in>
2025-06-23 17:51:24 +0530

    request code added

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template,  request
+import requests
+
+app = Flask(__name__)
+
+GITHUB_API = "https://api.github.com/users/"
+
+@app.route('/', methods=['GET', 'POST'])
+
+def index():
+    if request.method == 'POST':
+        username = request.form('username')
+        user_url = f"{GITHUB_API}{username}"
+        repo_url = f"{user_url}/repos"
+
+        user_data = requests.get(user_url).json()
+        repo_data = requests.get(repo_url).json()
+
+        if "message" in user_data and user_data ["message"] == "Not Found":
+            return render_template('index.html', error = "User not found")
+        
+
+
